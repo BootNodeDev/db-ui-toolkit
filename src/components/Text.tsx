365d95e945fea79d4abab3c330698bba207b4a37
Gabito Esmiapodo <gabriel@bootnode.dev>
2024-05-29 17:34:41 -0300

    refactor: update var names

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -2,7 +2,7 @@ import { styled } from 'styled-components'
 
 const Text = styled.p`
   color: var(--theme-color-text, #000);
-  font-size: var(--base-text-size, 16px);
+  font-size: var(--base-text-font-size, 16px);
   font-weight: 400;
   line-height: 1.4;
   margin: 0 0 calc(var(--base-gap, 8px) * 2);
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,7 +2,7 @@ import { styled } from 'styled-components'
 
 const Title = styled.h1`
   color: var(--theme-color-text, #000);
-  font-size: var(--base-title-size, 24px);
+  font-size: var(--base-title-font-size, 24px);
   font-weight: 700;
   line-height: 1.2;
   margin: 0 0 calc(var(--base-gap, 8px) * 3);

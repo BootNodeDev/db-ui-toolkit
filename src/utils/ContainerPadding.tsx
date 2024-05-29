ad75100040719f280ec835c9071135fe7e87ddad
Gabito Esmiapodo <gabriel@bootnode.dev>
2024-05-29 10:45:46 -0300

    chore: update var names

--- src/utils/ContainerPadding.tsx
+++ src/utils/ContainerPadding.tsx
@@ -6,38 +6,38 @@ import { breakpointMediaQuery } from './breakpoints'
  * Responsive container padding for all breakpoints
  */
 export const ContainerPadding = css`
-  padding-left: var(--base-padding-sm, 4px);
-  padding-right: var(--base-padding-sm, 4px);
+  padding-left: var(--base-padding-mobile, 4px);
+  padding-right: var(--base-padding-mobile, 4px);
 
   ${breakpointMediaQuery(
     'tabletPortraitStart',
     css`
-      padding-left: var(--base-padding, 8px);
-      padding-right: var(--base-padding, 8px);
+      padding-left: var(--base-padding-tablet-portrait, 8px);
+      padding-right: var(--base-padding-tablet-portrait, 8px);
     `,
   )}
 
   ${breakpointMediaQuery(
     'tabletLandscapeStart',
     css`
-      padding-left: var(--base-padding, 8px);
-      padding-right: var(--base-padding, 8px);
+      padding-left: var(--base-padding-tablet-landscape, 8px);
+      padding-right: var(--base-padding-tablet-landscape, 8px);
     `,
   )}
 
   ${breakpointMediaQuery(
     'desktopStart',
     css`
-      padding-left: var(--base-padding-xl, 16px);
-      padding-right: var(--base-padding-xl, 16px);
+      padding-left: var(--base-padding-desktop, 16px);
+      padding-right: var(--base-padding-desktop, 16px);
     `,
   )}
 
   ${breakpointMediaQuery(
     'desktopWideStart',
     css`
-      padding-left: var(--base-padding-xl, 16px);
-      padding-right: var(--base-padding-xl, 16px);
+      padding-left: var(--base-padding-desktop-xl, 16px);
+      padding-right: var(--base-padding-desktop-xl, 16px);
     `,
   )}
 `

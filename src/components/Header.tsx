d59fd9a8676109fc44e467f7170cca0d24b39d15
Gabito Esmiapodo <gabriel@bootnode.dev>
2024-05-29 12:00:44 -0300

    refactor: delete superfluous space

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,7 +17,7 @@ const Header: React.FC<Props> = ({ children, style, className, ...restProps }) =
   return (
     <Wrapper
       style={style}
-      className={`dbuitkHeader  ${className ? className : ''}`.trim()}
+      className={`dbuitkHeader ${className ? className : ''}`.trim()}
       {...restProps}
     >
       {children}

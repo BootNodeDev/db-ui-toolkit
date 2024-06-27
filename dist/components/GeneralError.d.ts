39563d45e74fa9d41f97debc824f663f35c405d2
Gabito Esmiapodo <gabriel@bootnode.dev>
2024-06-27 16:55:16 -0300

    chore: build

--- dist/components/GeneralError.d.ts
+++ dist/components/GeneralError.d.ts
@@ -15,10 +15,13 @@ interface Props {
  *
  * Theme CSS variables:
  *
+ * --theme-generic-error-background-color: Background color of the container.
+ * --theme-generic-error-border-color: Border color of the container.
+ * --theme-generic-error-box-shadow: Box shadow of the container.
+ * --theme-generic-error-color-icon: Color of the icon (only works for fill="currentColor").
  * --theme-generic-error-color-title: Color of the title.
- * --theme-generic-error-color-text: Color of the text.
- * --theme-generic-error-color-icon: Color of the icon.
- * --theme-generic-error-color-message-background: Background color of the message.
+ * --theme-generic-error-color-text: Color of the text message.
+ * --theme-generic-error-color-message-background: Background color of the text message.
  */
 declare const GeneralError: React.FC<Props>;
 export default GeneralError;

647f87e0a932561c8d51707a26a4411d5fcad1dc
Gabito Esmiapodo <gabriel@bootnode.dev>
2024-06-27 16:52:03 -0300

    fix: card default shadow

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,7 +13,7 @@ const Card = styled.div.attrs(({ className = 'dbuitkCard' }) => ({ className }))
   background-color: var(--theme-card-background-color, #fff);
   border-radius: var(--base-border-radius, 8px);
   border: 1px solid var(--theme-card-border-color, #fff);
-  box-shadow: var(--theme-card-box-shadow, 0 9.6px 13px 0 rgb(0 0 0 / 8%));
+  box-shadow: var(--theme-card-box-shadow, 0 0 20px 0 rgb(0 0 0 / 8%));
   display: flex;
   flex-direction: column;
   padding: var(--base-common-padding-xl, 16px);

f4c1cdfdc2eee5d529f6655eb2c071d72ab8be19
Gabito Esmiapodo <gabriel@bootnode.dev>
2024-06-28 14:08:46 -0300

    chore: build

--- dist/components/Textfield.d.ts
+++ dist/components/Textfield.d.ts
@@ -14,15 +14,15 @@ export declare const TextfieldCSS: import("styled-components").RuleSet<Props>;
  *
  * Theme CSS vars:
  *
- * * --theme-textfield-default-background-color
- * * --theme-textfield-default-background-color-active
- * * --theme-textfield-default-border-color
- * * --theme-textfield-default-color
- * * --theme-textfield-default-error-color
- * * --theme-textfield-default-ok-color
- * * --theme-textfield-default-placeholder-color
- * * --theme-textfield-default-border-error-color
- * * --theme-textfield-default-border-ok-color
+ * * --theme-textfield-background-color
+ * * --theme-textfield-background-color-active
+ * * --theme-textfield-border-color
+ * * --theme-textfield-color
+ * * --theme-textfield-error-color
+ * * --theme-textfield-ok-color
+ * * --theme-textfield-placeholder-color
+ * * --theme-textfield-border-error-color
+ * * --theme-textfield-border-ok-color
  * * --theme-textfield-active-boxshadow
  *
  * Base CSS vars:
@@ -30,8 +30,8 @@ export declare const TextfieldCSS: import("styled-components").RuleSet<Props>;
  * * --base-textfield-height
  * * --base-textfield-border-radius
  * * --base-textfield-font-size
- * * --base-textfield-default-vertical-padding
- * * --base-textfield-default-horizontal-padding
+ * * --base-textfield-vertical-padding
+ * * --base-textfield-horizontal-padding
  * * --base-textfield-transition-time
  */
 declare const Textfield: import("styled-components/dist/types").IStyledComponentBase<"web", import("styled-components/dist/types").Substitute<import("react").DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, Props>> & string;

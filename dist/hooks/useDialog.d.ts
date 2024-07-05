5d7b00330fb36856cd390c643338b9ff26c374ae
Gabito Esmiapodo <gabriel@bootnode.dev>
2024-07-05 20:50:57 -0300

    chore: build

--- dist/hooks/useDialog.d.ts
+++ dist/hooks/useDialog.d.ts
@@ -1,8 +1,9 @@
-import { type FC, type HTMLAttributes, ReactElement } from 'react';
+import { ReactElement, type FC, type HTMLAttributes } from 'react';
 interface DialogProps extends HTMLAttributes<HTMLDialogElement> {
 }
 interface Options extends DialogProps {
     closeOnOutsideClick?: boolean;
+    closeOnEscape?: boolean;
 }
 interface Props {
     Dialog: FC<Options>;
@@ -13,10 +14,13 @@ interface Props {
  * @name useDialog
  * @description A hook to create and use a dialog component.
  *
+ * @returns {Props} {open: (children: ReactElement) => void, close: () => void, Dialog: FC<Options>} - The `Dialog` component and the hook's methods.
+ *
+ * **Dialog props**
+ *
  * @param {Options} options - The options for the dialog.
  * @param {boolean} [options.closeOnOutsideClick=true] - Whether the dialog should close when clicking outside of it. Defaults to true.
- *
- * @returns {Props} {open: (children: ReactElement) => void, close: () => void, Dialog: FC<ModalProps>} - The `Dialog` component and the hook's methods.
+ * @param {boolean} [options.closeOnEscape=true] - Whether the dialog should close when pressing the escape key. Defaults to true.
  *
  * **Base CSS Variables for `Dialog` Component**
  *

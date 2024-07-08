96a022371639053922440e36631fc3ad2bb7207e
Gabito Esmiapodo <gabriel@bootnode.dev>
2024-07-08 10:38:19 -0300

    docs: update dialog docs

--- src/components/Dialog/index.tsx
+++ src/components/Dialog/index.tsx
@@ -26,6 +26,12 @@ interface Props extends DialogProps {
  *
  * @description A dialog component that can be opened and closed.
  *
+ * @param {ReactNode} children - The content of the dialog.
+ * @param {boolean} [closeOnEscape=true] - Whether the dialog should close when the escape key is pressed. Defaults to true.
+ * @param {boolean} [closeOnOutsideClick=true] - Whether the dialog should close when clicked outside of it. Defaults to true.
+ * @param {boolean} isOpen - Whether the dialog is open.
+ * @param {() => void} onClose - A callback function triggered when the dialog is closed.
+ *
  * **Base CSS Variables for `Dialog` Component**
  *
  * - `--base-dialog-border-radius` (set it the same as the children's in open(children) or the radius of the children will be ignored)

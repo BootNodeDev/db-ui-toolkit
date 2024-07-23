d67aa44b0c58727f50812a6e755004a7103ee29e
Gabito Esmiapodo <gabriel@bootnode.dev>
2024-07-23 12:37:32 -0300

    docs: update

--- src/components/GeneralErrorDialog.tsx
+++ src/components/GeneralErrorDialog.tsx
@@ -69,9 +69,9 @@ interface Props extends GeneralErrorProps {
 }
 
 /**
- * General error component shown in an HTML dialog element.
+ * @name GeneralErrorDialog
  *
- * Shares the same props as the GeneralError component plus the following:
+ * @description General error component shown in an HTML dialog element. It shares the same props as the GeneralError component plus the following:
  *
  * @param {() => void}  [$onClose] - An optional function to be called when the dialog is closed.
  * @param {boolean}     [$closeOnOutsideClick] - If true, the dialog will close when clicking outside of it. Default is true.

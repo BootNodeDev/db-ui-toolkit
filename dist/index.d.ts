c5225f308a3cbf7caefa1301a2b6fee0a4e1d80c
Gabito Esmiapodo <gabriel@bootnode.dev>
2024-07-31 17:13:47 -0300

    chore: build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,11 +2,10 @@
  * Exported components
  */
 export { default as Button, ActiveButtonCSS, ButtonCSS, ButtonVariantCSS, DisabledButtonCSS, } from './components/Button';
-export { Dropdown, Item, Items, type Direction, type DropdownExposedProps, type Position, } from './components/Dropdown';
-export { TextfieldCSS, type TextfieldStatus, type Props as TextfieldProps, } from './components/Textfield';
 export { default as Card } from './components/Card';
 export { default as CopyButton } from './components/CopyButton';
 export { default as Dialog } from './components/Dialog';
+export { Dropdown, Item, Items, type Direction, type DropdownExposedProps, type Position, } from './components/Dropdown';
 export { default as ExternalLink } from './components/ExternalLink';
 export { default as Footer } from './components/Footer';
 export { default as GeneralMessage } from './components/GeneralMessage';
@@ -17,10 +16,11 @@ export { default as Logo } from './components/Logo';
 export { default as LogoMini } from './components/LogoMini';
 export { default as Main } from './components/Main';
 export { default as Modal } from './components/Modal';
+export { default as SkeletonLoading } from './components/SkeletonLoading';
 export { default as Spinner } from './components/Spinner';
 export { default as SwitchThemeButton } from './components/SwitchThemeButton';
 export { default as Text } from './components/Text';
-export { default as Textfield } from './components/Textfield';
+export { default as Textfield, TextfieldCSS, type TextfieldStatus, type Props as TextfieldProps, } from './components/Textfield';
 export { default as Title } from './components/Title';
 export { default as Toast } from './components/Toast';
 export { default as Wrapper } from './components/Wrapper';
